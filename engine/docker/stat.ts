bced4803e603d889736f0f1a17bf310b863e418d
ZorTik <zortlegit@gmail.com>
2024-05-17 19:55:30 +0200

    Nullable types on engine container stat

--- engine/docker/stat.ts
+++ engine/docker/stat.ts
@@ -2,9 +2,9 @@ import {ServiceEngine} from "../engine";
 import DockerClient from "dockerode";
 
 function calcCpuUsage(precpu: DockerClient.CPUStats, cpu: DockerClient.CPUStats) {
-    const cpu_delta = cpu.cpu_usage.total_usage - precpu.cpu_usage.total_usage;
-    const system_cpu_delta = cpu.system_cpu_usage - precpu.system_cpu_usage;
-    const number_cpus = cpu.online_cpus;
+    const cpu_delta = cpu?.cpu_usage.total_usage - precpu?.cpu_usage.total_usage;
+    const system_cpu_delta = cpu?.system_cpu_usage - precpu?.system_cpu_usage;
+    const number_cpus = cpu?.online_cpus;
     return (cpu_delta / system_cpu_delta) * number_cpus * 100.0;
 }
 
@@ -14,13 +14,13 @@ export default function (self: ServiceEngine, client: DockerClient): ServiceEngi
         return {
             id,
             memory: {
-                used: memory_stats.usage,
-                total: memory_stats.limit,
-                percent: memory_stats.usage / memory_stats.limit,
+                used: memory_stats?.usage,
+                total: memory_stats?.limit,
+                percent: memory_stats?.usage / memory_stats?.limit,
             },
             cpu: {
-                used: cpu_stats.cpu_usage.total_usage,
-                total: cpu_stats.system_cpu_usage,
+                used: cpu_stats?.cpu_usage.total_usage,
+                total: cpu_stats?.system_cpu_usage,
                 //percent: cpu_stats.cpu_usage.total_usage / cpu_stats.system_cpu_usage,
                 percent: calcCpuUsage(precpu_stats, cpu_stats),
             },
