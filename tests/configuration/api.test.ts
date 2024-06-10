646161ffd8f70e9dba496632b391cda3c0173220
ZorTik <zortlegit@gmail.com>
2024-06-10 12:42:39 +0200

    revert thingy

--- tests/configuration/api.test.ts
+++ tests/configuration/api.test.ts
@@ -24,7 +24,7 @@ describe("Test v1 API", () => {
         const res = await request(server).get("/v1/status?stats=true");
         expect(res.status).toBe(200);
         expectProps(res.body, [
-            'stats.memory.usedt',
+            'stats.memory.used',
             'stats.memory.total',
             'stats.memory.percent',
             'stats.cpu.used',
@@ -35,4 +35,6 @@ describe("Test v1 API", () => {
             'stats.services.diskTotal',
         ]);
     });
+
+    // TODO: Add missing API tests
 });
