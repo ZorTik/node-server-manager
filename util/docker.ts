65f17fc1a572d64b0e0419b14f4b173b5db505f0
ZorTik <zortlegit@gmail.com>
2024-07-31 15:14:01 +0200

    Cpu usage zero on null

--- util/docker.ts
+++ util/docker.ts
@@ -4,7 +4,7 @@ function calcCpuUsage(precpu: DockerClient.CPUStats, cpu: DockerClient.CPUStats)
     const cpu_delta = cpu?.cpu_usage.total_usage - precpu?.cpu_usage.total_usage;
     const system_cpu_delta = cpu?.system_cpu_usage - precpu?.system_cpu_usage;
     const number_cpus = cpu?.online_cpus;
-    return (cpu_delta / system_cpu_delta) * number_cpus * 100.0;
+    return ((cpu_delta / system_cpu_delta) * number_cpus * 100.0) ?? 0.0;
 }
 
 export function adaptContainerStatsFromDocker(id: string, stats: ContainerStats) {
