a6e6e7d8bd55e8ac5f00d0866aa42e77c3bdde35
ZorTik <zortlegit@gmail.com>
2024-08-27 15:10:11 +0200

    Add required option to env util

--- util/env.ts
+++ util/env.ts
@@ -1,15 +1,18 @@
-export function env(env: string[]) {
+export function env(env: string[], options?: { required?: boolean }) {
+    const required = options?.required ?? true;
     const values = [];
     const missing = [];
     for (let i = 0; i < env.length; i++) {
         const key = env[i];
+        let value = undefined;
         if (key in process.env) {
-            values.push(process.env[key]);
+            value = process.env[key];
         } else {
             missing.push(key);
         }
+        values.push(value);
     }
-    if (missing.length > 0) {
+    if (missing.length > 0 && required == true) {
         throw new Error('Missing env variables: ' + missing.join(', '));
     }
     return values;
