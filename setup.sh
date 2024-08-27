12a9e3bb737b14fbca972dcd71c808f0a04b5fca
ZorTik <zortlegit@gmail.com>
2024-08-27 23:05:29 +0200

    Delete setup.sh, TODO

--- setup.sh
+++
@@ -1,8 +0,0 @@
-npm install
-npx prisma generate
-npx prisma migrate dev --name $1
-echo "------------------------------------"
-echo "Dependencies setup complete!"
-echo "Please configure .env from .env.example"
-echo "and config.yml."
-echo "------------------------------------"
