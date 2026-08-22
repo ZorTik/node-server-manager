0750cd82fb5096e873c5386759eac712f3700add
JustDrven <petr.nemec11111@gmail.com>
2026-08-22 16:22:39 +0200

    refactor: replace from duplicate strings to constants

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,15 @@
 import winston from "winston";
 import fs from "fs";
+import { API_LOGS_PATH, SERVICES_LOGS_PATH } from "./log";
 
 const { combine, timestamp, label, printf } = winston.format;
 
 export function createNewLatest() {
-    if (fs.existsSync(process.cwd() + '/logs/latest.log')) {
+    if (fs.existsSync(API_LOGS_PATH + '/latest.log')) {
         const date = new Date(Date.now()).toJSON().slice(2, 10) + '.'
             + new Date(Date.now()).getHours() + '.'
             + new Date(Date.now()).getMinutes();
-        fs.renameSync(process.cwd() + '/logs/latest.log', process.cwd() + '/logs/' + date + '.log');
+        fs.renameSync(API_LOGS_PATH + '/latest.log', process.cwd() + '/logs/' + date + '.log');
     }
 }
 
@@ -25,13 +26,14 @@ export function createLogger(options?: { label?: string }) {
         ),
         transports: [
             new winston.transports.Console(),
-            new winston.transports.File({dirname: 'logs', filename: 'latest.log'})
+            new winston.transports.File({dirname: API_LOGS_PATH, filename: 'latest.log'})
         ]
     });
 }
 
 export function logService(id: string, str: any) {
     // Isn't this thing blocking??? Look at it later, zort - by zort xdd
-    const log_path = process.cwd() + '/service_logs/' + id + '.log';
+    const log_path = SERVICES_LOGS_PATH + id + '.log';
     fs.appendFileSync(log_path, (str ?? '').toString() + '\n');
 }
+
