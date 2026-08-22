6ef9c6cd7f9355405b76abcc5cafa88a07ebbbd9
JustDrven <petr.nemec11111@gmail.com>
2026-08-22 17:00:58 +0200

    feat: add send plain text in util

--- src/router/util/preconditions.ts
+++ src/router/util/preconditions.ts
@@ -23,3 +23,7 @@ export const checkServicePending = (serviceId: string, res: express.Response) =>
 
   return true;
 };
+
+export const sendPlainText = (res: express.Response, data: string) => {
+  return res.status(200).type("text/plain; charset=utf-8").send(data);
+}
